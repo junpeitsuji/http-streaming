0f13756f510d81cdd981ae23a16e28f1964999bc
Junpei Tsuji <tsuji@complex.ist.hokudai.ac.jp>
2013-12-08 11:40:00 +0900

    push.php に直接アクセスされたときに、トップページへ飛ばすように変更

--- push.php
+++ push.php
@@ -7,36 +7,60 @@ function output_chunk($chunk)
     echo $chunk . "\r\n";
 }
 
-header("Content-type: application/octet-stream");
-header("Transfer-encoding: chunked");
-flush();
+/** 
+ * Ajaxによるリクエストかどうか 
+ * 
+ * @return boolean True or False 
+ */  
+function isAjax()  
+{  
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){  
+        return true;  
+    }  
+    return false;  
+}  
 
-$i = 0;
-while (true) {
-    
-    // JSON に入れるデータ
-    $id        = $i;
-    $message   = "hoge".$i;
-	$createdAt = date("Y-m-d H:i:s");
 
-    // JSON データを作る
-    $json = json_encode( 
-        array(
-            "id" => $id, 
-            "message" => $message, 
-            "createdAt" => $createdAt
-        ) 
-    ); 
+if( isAjax() ){
+    // Ajax のアクセスのみ受け付ける
 
-    // データを掃き出す
-    output_chunk(
-        $json . str_repeat(' ', 8000) . "\n"
-    );
+    header("Content-type: application/octet-stream");
+    header("Transfer-encoding: chunked");
     flush();
 
-    // 一秒停止
-    sleep(1); // 一秒ごとに生成
+    $i = 0;
+    while ( !connection_aborted() ) {
+        
+        // JSON に入れるデータ
+        $id        = $i;
+        $message   = "hoge".$i;
+        $createdAt = date("Y-m-d H:i:s");
 
-    $i++;
+        // JSON データを作る
+        $json = json_encode( 
+            array(
+                "id" => $id, 
+                "message" => $message, 
+                "createdAt" => $createdAt
+            ) 
+        ); 
+
+        // データを掃き出す
+        output_chunk(
+            $json . str_repeat(' ', 8000) . "\n"
+        );
+        ob_flush();
+        flush();
+
+        // 一秒停止
+        sleep(1); // 一秒ごとに生成
+
+        $i++;
+    }
+    //echo "0\r\n\r\n";    
 }
-//echo "0\r\n\r\n";
+else {
+    // Ajax アクセス以外はトップページへ誘導
+    header("Location: http://".$_SERVER["SERVER_NAME"]."/");
+
+}
